7512d449333e70dbd780b8ee5244d8838d2d470c
Eugen Zagorodniy <e.zagorodniy@gmail.com>
2011-12-15 14:33:10 +0200

    Do parse 'getq_update' arguments

--- getq/templatetags/getq.py
+++ getq/templatetags/getq.py
@@ -3,8 +3,8 @@ from django import template
 register = template.Library()
 
 class GetqUpdateNode(template.Node):
-    def __init__(self):
-        pass
+    def __init__(self, params):
+        self.new_params = params
 
     def render(self, context):
         try:
@@ -14,8 +14,21 @@ class GetqUpdateNode(template.Node):
                 "No request available in context (perhaps request context "
                 "processor is disabled?)"
             )
-        return 'hello-world'
+        params = request.GET.copy()
+        # Not using QueryDict.update() because it does append updated
+        # values to existing ones rather then replace them.
+        for param, value in self.new_params.iteritems():
+            params[param] = value
+        return '%s?%s' % (request.path, params.urlencode())
 
 @register.tag
 def getq_update(parser, token):
-    return GetqUpdateNode()
+    contents = token.split_contents()
+    tag_name = contents[0]
+    statements = contents[1:]
+    if not statements:
+        raise template.TemplateSyntaxError(
+            "'%s' tag requires 1 or more arguments" % tag_name
+        )
+    params = dict(s.split('=', 1) for s in statements)
+    return GetqUpdateNode(params)
